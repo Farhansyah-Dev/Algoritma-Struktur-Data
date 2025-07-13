09f6f88f0fe46f6d7bf38e61ca3f6c4f3138e3ca
Farhansyah-Dev <farhansyah.it@gmail.com>
2025-07-13 14:04:28 +0700

    Membuat program Constant

---
+++ 1. Pengenalan Algortima/Bahasa C/Constant.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+
+    int myNumber;
+    int MyBirtday;
+    const int myNumber = 12;
+    myNumber = 13; //Erorr Result karena nilai Constant tidak bisa dirubah.
+
+    /*Praktik yang baik dengan menggunakan nama variable dengan huruf kapital
+    diawal
+    example:
+    */
+    const MYBIRTDAY = 2004;
+
+printf ("%d myNumber is: ", myNumber);
+}
+
